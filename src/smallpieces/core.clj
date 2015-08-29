e0038c4b1dc3be7346865646156dbec2247dec21
Jarrod Swart <jcswart@gmail.com>
2015-08-29 14:17:33 -0400

    Add initial functions for simple evaluate.

--- src/smallpieces/core.clj
+++ src/smallpieces/core.clj
@@ -1,13 +1,37 @@
 (ns smallpieces.core)
 
-;; Need to def/alias
-;; atom?, car, cdr, boolean?, wrong
+(def car first)
+(def cdr last)
 
-(defn evaluate [expr env]
-  (if (atom? expr)
-    (cond (symbol? expr) (lookup expr env)
-          (or (number? expr) (string? expr) (char? expr)
-              (boolean? expr) (vector? expr)) expr
-          :else (wrong "Can't evaluate" expr))
-    (case (car expr)
-      ...)))
+(defn wrong
+  [msg v]
+  (throw (ex-info msg {:data v})))
+
+(defn boolean?
+  [x]
+  (or (= true x) (= false x)))
+
+(defn atom?
+  [x]
+  (let [pair? (fn [x]
+                (or (= (type x) clojure.lang.Cons)
+                    (list? x)))]
+    (and (not (pair? x))
+         (not (nil? x)))))
+
+(defn lookup
+  [e env]
+  (println e env) e)
+
+(defn evaluate [e env]
+  (if (atom? e)
+    (cond (symbol? e)      (lookup e env)
+          (or (number?  e)
+              (string?  e)
+              (char?    e)
+              (boolean? e)
+              (vector?  e)) e
+          :else (wrong "Can't evaluate" e))
+    (case (car e)
+      e)
+    ))
